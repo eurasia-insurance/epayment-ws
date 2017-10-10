2bc2a051e5f146ec5d97b933758f60e4597665a0
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-10 17:16:54 +0600

    new entity ebill request and passing return page

--- src/main/java/tech/lapsa/epayment/ws/jaxb/entity/XmlEbillRequest.java
+++ src/main/java/tech/lapsa/epayment/ws/jaxb/entity/XmlEbillRequest.java
@@ -1,35 +1,39 @@
 package tech.lapsa.epayment.ws.jaxb.entity;
 
-import java.io.Serializable;
+import java.net.URI;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import com.lapsa.validation.NotEmptyString;
 import com.lapsa.validation.NotNullValue;
 
-import tech.lapsa.epayment.ws.jaxb.validator.ValidEbillId;
+import tech.lapsa.java.jaxb.adapter.XmlURIAdapter;
 
-@XmlRootElement(name = "ebillShort")
+@XmlRootElement(name = "ebillRequest")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class XmlEbillShort implements Serializable {
+public class XmlEbillRequest extends XmlEbillShort {
     private static final long serialVersionUID = 1L;
 
     @XmlAttribute
     @NotNullValue
-    @NotEmptyString
-    @ValidEbillId
-    protected String id;
+    @XmlJavaTypeAdapter(XmlURIAdapter.class)
+    protected URI returnUri;
 
-    public XmlEbillShort() {
+    public XmlEbillRequest() {
     }
 
-    public XmlEbillShort(String id) {
-	this.id = id;
+    public XmlEbillRequest(String id) {
+	super(id);
+    }
+
+    public XmlEbillRequest(String id, URI returnUri) {
+	super(id);
+	this.returnUri = returnUri;
     }
 
     @Override
@@ -37,12 +41,11 @@ public class XmlEbillShort implements Serializable {
 	return ToStringBuilder.reflectionToString(this, Constants.DEFAULT_TO_STRING_STYLE);
     }
 
-    public String getId() {
-	return id;
+    public URI getReturnUri() {
+	return returnUri;
     }
 
-    public void setId(String id) {
-	this.id = id;
+    public void setReturnUri(URI returnUri) {
+	this.returnUri = returnUri;
     }
-
 }
--- src/main/java/tech/lapsa/epayment/ws/jaxb/validator/constraint/ValidEbillIdConstraintValidator.java
+++ src/main/java/tech/lapsa/epayment/ws/jaxb/validator/constraint/ValidEbillIdConstraintValidator.java
@@ -7,27 +7,26 @@ import javax.validation.ValidationException;
 import com.lapsa.epayment.facade.EpaymentFacade;
 import com.lapsa.utils.BeanUtils;
 
-import tech.lapsa.epayment.ws.jaxb.entity.XmlEbillShort;
-import tech.lapsa.epayment.ws.jaxb.validator.EbillValid;
+import tech.lapsa.epayment.ws.jaxb.validator.ValidEbillId;
 
-public class EbillValidConstraintValidator implements ConstraintValidator<EbillValid, XmlEbillShort> {
+public class ValidEbillIdConstraintValidator implements ConstraintValidator<ValidEbillId, String> {
 
     @Override
-    public void initialize(EbillValid constraintAnnotation) {
+    public void initialize(ValidEbillId constraintAnnotation) {
     }
 
     @Override
-    public boolean isValid(XmlEbillShort value, ConstraintValidatorContext context) {
+    public boolean isValid(String value, ConstraintValidatorContext context) {
 	if (value == null)
 	    return true;
 	try {
 	    BeanUtils.lookup(EpaymentFacade.class) //
 		    .orElseThrow(() -> new ValidationException("Cannot find an instance of EpaymentFacade")) //
 		    .newEbillFetcherBuilder() //
-		    .usingId(value.getId()); // it should throws
-						 // IllegalArgumentException on
-						 // invalid id or nonexistent
-						 // entity
+		    .usingId(value); // it should throws
+				     // IllegalArgumentException on
+				     // invalid id or nonexistent
+				     // entity
 	    return true;
 	} catch (IllegalArgumentException e) {
 	    return false;
