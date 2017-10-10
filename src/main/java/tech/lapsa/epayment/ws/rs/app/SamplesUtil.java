c52708a5b2940a3895e1c30316e79caa7095d0a3
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-10 18:45:56 +0600

    sample params changed

--- src/main/java/tech/lapsa/epayment/ws/rs/app/SamplesUtil.java
+++ src/main/java/tech/lapsa/epayment/ws/rs/app/SamplesUtil.java
@@ -45,8 +45,8 @@ public class SamplesUtil {
 			    new XmlHttpFormParam("Signed_Order_B64",
 				    "PGRvY3VtZW50PjxtZXJjaGFudCBjZXJ0X2lkPSI3MWY4MDQxMCIgbmFtZT0iZXVyYXNpYTM2Lmt6Ij48b3JkZXIgb3JkZXJfaWQ9IjgzMzgzNTgyOTg5Njc0NCIgY3VycmVuY3k9IjM5OCIgYW1vdW50PSIxMzcxMS44MiI+PGRlcGFydG1lbnQgbWVyY2hhbnRfaWQ9Ijk4MTE2NjUxIiBhbW91bnQ9IjEzNzExLjgyIi8+PC9vcmRlcj48L21lcmNoYW50PjxtZXJjaGFudF9zaWduIHR5cGU9IlJTQSI+a0hQSzRsY1krZTF0QWpKZlQycVRwdG1oZVVxS2pkNkdTb2hyMk1sdXkxUzMvak9tSDVWVEpza1pPUENGUGNGM0gxZUtmU2tZc3FJdkhuVjF6blhnTVQzWjhEQkx5dWVrQk93d29tRjJJTlpscFltZ1F6cE5WbkJVSzY4eG5Uemxpa0JMMjYxRmI3SmFvbWlhT29DNDNtcWl6R0ZDaWI1UnRJQjNHS3V4OHdYTG1qbThCRGJaZ1lzSEhGNHYrc3BaVjhsSU14aUpiN3JreG1DSCtUeVNBQlNrRnFmYm9acm5EbzhXMklSRnFiU0p6cWc3WDlpQzhwTDZvRndjT2NhekhuY1NWbXRsNkc5Q081K0h4OXEzaG9UcW9WVXJwSEFPdjg3YnZhaUlZa3A0V3M3UTYwTVRGaFRMQmVJUUtpWUN1Z2pUcHJLTmRhMGY2d3FUNlYrMkN3PT08L21lcmNoYW50X3NpZ24+PC9kb2N1bWVudD4="), //
 			    new XmlHttpFormParam("template", "default.xsl"), //
-			    new XmlHttpFormParam("email", "vadim.o.isaev@gmail.com"), //
-			    new XmlHttpFormParam("PostLink", "https://isaev.theeurasia.kz:8181/order/ws/qazkom/ok"), //
+			    new XmlHttpFormParam("email", "jonn@gmail.com"), //
+			    new XmlHttpFormParam("PostLink", "https://localhost:8181/order/ws/qazkom/ok"), //
 			    new XmlHttpFormParam("Language", "%%LANGUAGE_TAG%%"), //
 			    new XmlHttpFormParam("appendix",
 				    "PGRvY3VtZW50PjxpdGVtIG5hbWU9IlBvbGljeSBvZiBPSSBDTFZPIiBudW1iZXI9IjEiIHF1YW50aXR5PSIxIiBhbW91bnQ9IjEzNzExLjgyIi8+PC9kb2N1bWVudD4="),
